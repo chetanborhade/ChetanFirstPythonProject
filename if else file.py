fbd9a3b4bab82e8f60ba6ad7ee542ef93fd00a41
Chetan Borhade <mail2chetan03@gmail.com>
2018-04-06 17:14:14 +0530

    committed

---
+++ if else file.py
@@ -0,0 +1,10 @@
+check = "burgerr"
+
+if check == False:
+    print("It is false")
+elif check == "burgerr":
+    print("Yummmm, burgerr")
+elif check == "Hi":
+    print("Hello")
+else:
+    print("It is actually equal to true")
