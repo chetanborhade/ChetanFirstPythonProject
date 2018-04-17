b5cb3d2cc0e9518d83f850f6c58fbb1c4bd38011
Chetan Borhade <mail2chetan03@gmail.com>
2018-04-17 17:26:43 +0530

    committed

--- main.py
+++ main.py
@@ -1,107 +1,16 @@
-from Classes.game import bcolors, Person
-from Classes.magic import Spell
-from Classes.inventory import Item
+import json
+import os
 
+if os.path.isfile("./ages.json") and os.stat("./ages.json").st_size != 0:
+    old_file = open("./ages.json", "r+")
+    data = json.loads(old_file.read())
+    print("current age is", data["age"], "- adding a year.")
+    data["age"] - data["age"] + 1
+    print("New age is", data["age"])
+else:
+    old_file = open("./ages.json", "w+")
+    data = {"name": "Chetan", "age": 29}
+    print("No file found, setting default is to", data["age"])
 
-#create black magic
-fire = Spell("Fire", 10, 100, "black")
-thunder = Spell("Thunder", 10, 100, "black")
-blizzard = Spell("Blizzard", 10, 100, "black")
-meteor = Spell("Meteor", 20, 200, "black")
-quake = Spell("Quake", 14, 140, "black")
-
-#create white magic
-cure = Spell("Cure", 12, 120, "white")
-cura = Spell("Cura", 18, 200, "white")
-
-#create some Items
-potion = Item("Potion", "potion", "Heals 50 HP", 50)
-hipotion = Item("Hi-Potion", "potion", "Heals 100 Hp", 100)
-superpotion = Item("Super potion", "potion", "Heals 500 Hp", 500)
-elixer = Item("Elixer", "elixer", "Fully restores HP/MP of one party member", 9999)
-hielixer = Item("MegaElixer", "elixer", "Fully restores party's HP/MP", 9999)
-
-grenade = Item("Grenade", "attacks", "Deals 500 damage", 500)
-
-player_spells = [fire, thunder, blizzard, meteor, cure, cura]
-player__items = [potion, hipotion, superpotion, elixer, hielixer, grenade]
-
-
-#Instantiate People
-player = Person(460, 65, 60, 34, player_spells, player__items)
-enemy = Person(1200, 65, 45, 25, [], [])
-
-running = True
-i = 0
-
-print(bcolors.FAIL + bcolors.BOLD + "AN ENEMY ATTACKS!" + bcolors.ENDC)
-
-while running:
-    print("==============================")
-    player.choose_action()
-    choice = input("Choose action: ")
-    index = int(choice) - 1
-
-    if index == 0:
-        dmg = player.generate_damage()
-        enemy.take_damage(dmg)
-        print("You attacked for", dmg, "points of damage.")
-    elif index == 1:
-        player.choose_magic()
-        magic_choice = int(input("choose magic: ")) - 1
-
-        if magic_choice == -1:
-            continue
-
-        item =  player.items[item_choice]
-
-        if item.type == "potion":
-            player.heal(item.prop)
-            print(bcolors.OKGREEN + "\n" + item.name + "heals for", str(item.prop), "HP" + bcolors.ENDC )
-
-        spell = player.magic[magic_choice]
-        magic_dmg = spell.generate_damage()
-
-        current_mp = player.get_mp()
-
-        if spell.cost > current_mp:
-            print(bcolors.FAIL + "\nNot enough MP\n" + bcolors.ENDC)
-            continue
-
-        player.reduce_mp(spell.cost)
-
-        if spell.type == "White":
-            player.heal(magic_dmg)
-            print(bcolors.OKBLUE + "\n" + spell.name + "heal for", str(magic_dmg), "HP." + bcolors.ENDC)
-        elif spell.type == "black":
-            enemy.take_damage(magic_dmg)
-            print(bcolors.OKBLUE + "\n" + spell.name + "deals", str(magic_dmg), "points of damage" + bcolors.ENDC)
-    elif index == 2:
-        player.choose_item()
-        item_choice = int(input("Choose item: ")) - 1
-
-        if item_choice == -1:
-            continue
-
-
-    enemy_choice = 1
-
-    enemy_dmg = enemy.generate_damage()
-    player.take_damage(enemy_dmg)
-    print("Enemy attacks for", enemy_dmg)
-
-    print("=========================")
-    print("Enemy HP:", bcolors.FAIL + str(enemy.get_hp()) + "/" + str(enemy.get_max_hp()) + bcolors.ENDC + "\n")
-
-    print("Your HP:", bcolors.OKGREEN + str(player.get_hp()) + "/" + str (player.get_max_hp()) + bcolors.ENDC)
-    print("Your MP:", bcolors.OKBLUE + str(player.get_mp()) + "/" + str (player.get_max_mp()) + bcolors.ENDC + "\n")
-
-
-
-
-    if enemy.get_hp() == 0:
-        print(blocker.OKGREEN + "You Win!" + bcolors.ENDC)
-        running = False
-    elif player.get_hp() == 0:
-        print(bcolors.FAIL + "Your enemy defeated you!" + bcolors.ENDC)
-        running = False
+old_file.seek(0)
+old_file.write(json.dumps(data))
