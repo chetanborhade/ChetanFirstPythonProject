7bd5118b1ed98b13886b46a3e3b2cec0fb55e4ef
chetanborhade <38101972+chetanborhade@users.noreply.github.com>
2018-05-08 18:37:29 +0530

    Add files via upload

---
+++ MainClass5.java
@@ -0,0 +1,20 @@
+class Central1
+{
+	int j=33;
+	static double p=22.53;
+}
+class MainClass5
+{
+	static void test()
+	{
+		System.out.println("TEST METHOD");
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("MAIN METHOD");
+		test();
+		Central1 c1 = new Central1();
+		System.out.println(" J VALUE IS: "+c1.j);
+	}	
+}	
+
